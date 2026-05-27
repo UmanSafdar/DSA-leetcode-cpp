829b6c6896c6bdaa0548a58f6f22184329727d79
Uman Safdar <umansafdar@gmail.com>
2026-05-27 15:22:46 +0500

    Time: 0 ms (100.00%) | Memory: 27.2 MB (79.55%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int i =0;
+        if(!s.empty()){
+            while(i< n/2){
+           swap(s[i],s[(n-1)-i]);
+            i++;
+        }
+        }
+        
+        
+    }
+};
