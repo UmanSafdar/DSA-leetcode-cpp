1b54013110556d6a01ffa3c0c8ec9d768251b343
Uman Safdar <umansafdar@gmail.com>
2026-05-29 17:20:35 +0500

    Time: 0 ms (100.00%) | Memory: 14.5 MB (29.04%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        set<int> result;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+       int i = 0;
+       int j = 0;
+       while(i<n1 && j<n2){
+        if(nums1[i]==nums2[j]){
+            result.insert(nums1[i]);
+           i++;
+           j++;
+           }
+            else{
+                if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+       }
+       return vector<int>(result.begin(), result.end());
+        }
+        
+    };
